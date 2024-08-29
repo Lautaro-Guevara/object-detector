c9d44b1b221848c7495cd070f1183c8254e45cb4
Lautaro-Guevara <lfguevara2001@gmail.com>
2024-08-28 22:02:12 -0300

    Primer commit - Estructura del proyecto

---
+++ config.py
@@ -0,0 +1,4 @@
+
+"""
+Archivo central donde almacenas configuraciones globales, como las credenciales de la base de datos, la ruta a los modelos preentrenados, y otras configuraciones importantes.
+"""
---
+++ main.py
@@ -0,0 +1,5 @@
+
+
+"""
+El archivo principal que inicializa la aplicación y ejecuta la lógica principal (por ejemplo, iniciar la detección y la conexión a la base de datos).
+"""
